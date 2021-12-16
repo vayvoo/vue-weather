a14b2cc4860465c0d55193ff1142570537f6a3ae
vayvoo <60880037+vayvoo@users.noreply.github.com>
2021-12-16 09:58:48 +0500

    removed todays date

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">Dushanba, 20 Yanvar 2022</div>
+          <!-- <div class="date">Dushanba, 20 Yanvar 2022</div> -->
         </div>
         <div class="weather-box">
           <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
